af1c6671a04b2bfc05f9583b967a25a24cd24daf
ShenVi <yvban@outlook.com>
2020-11-21 23:17:54 +0800

    0

---
+++ tcp2.py
@@ -0,0 +1,21 @@
+import socket
+import time
+  
+HOST = ''
+PORT = 8002
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+sock.bind((HOST, PORT)) 
+sock.listen(5) 
+while True: 
+    connection,address = sock.accept()
+    print ('IP Address：'+ str(address))
+    # print(connection.recv(1024))
+    try: 
+        connection.settimeout(10)
+        buf = connection.recv(1024)
+        print(buf.decode())
+        
+    except Exception as e:
+        print("Error:", e)
+        # print ('time out')
+    connection.close()
